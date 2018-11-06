2a6e63c2b0761d6a26bd244ef5e2c008b0331245
VitaliZahar <vitalizahar@gmail.com>
2018-11-06 14:09:21 -0800

    Change project organisation

---
+++ AnimationsTask/Constants/Consts.swift
@@ -0,0 +1,13 @@
+//
+//  Consts.swift
+//  AnimationsTask
+//
+//  Created by vitali on 11/6/18.
+//  Copyright © 2018 vitcopr. All rights reserved.
+//
+
+import Foundation
+
+struct Consts {
+    
+}
