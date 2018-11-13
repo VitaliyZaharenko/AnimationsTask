08eafcd02723db19ed18b7ca4f79f20634c8b956
VitaliZahar <vitalizahar@gmail.com>
2018-11-13 01:26:25 -0800

    Start implementing text appear/disappear animation

---
+++ AnimationsTask/Controllers/TextAppearAnimationController.swift
@@ -0,0 +1,19 @@
+//
+//  TextAppearanceAnimationController.swift
+//  AnimationsTask
+//
+//  Created by vitali on 11/12/18.
+//  Copyright © 2018 vitcopr. All rights reserved.
+//
+
+import UIKit
+
+class TextAppearanceAnimationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
