2ba841a2189cd96b2865047c8e4cf1938a3deb00
VitaliZahar <vitalizahar@gmail.com>
2018-11-20 06:36:16 -0800

    Implement color view

--- AnimationsTask/Controllers/ViewAppearDisappearAnimationController.swift
+++ AnimationsTask/Controllers/ViewAppearDisappearAnimationController.swift
@@ -15,41 +15,24 @@ class ViewAppearDisappearAnimationController: UIViewController {
     //MARK: - Views
     @IBOutlet weak var colorView: ColorView!
     
-    //MARK: - Properties
-    
-    private var tapGestureRecognizer: UIGestureRecognizer!
-    private var layer: CALayer!
     
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
-        colorView.addGestureRecognizer(tapGestureRecognizer)
-        
-        layer = CALayer()
-        layer.backgroundColor = UIColor.red.cgColor
-        let minDimen = layer.frame.size.width > layer.frame.size.height ? layer.frame.size.height : layer.frame.size.width
-        
-        layer.cornerRadius = 30
-        layer.frame = colorView.bounds
-        layer.setAffineTransform(CGAffineTransform(scaleX: 0.1, y: 0.1))
-        colorView.layer.addSublayer(layer)
-        
     }
     
-    //MARK: - Callbacks
     
-    @objc private func viewTapped(){
+    //MARK: Actions
+    
+    
+    @IBAction func animate(_ sender: UIButton) {
         
-        
+        if(colorView.isVisible){
+            colorView.animateDisappear()
+        } else {
+            colorView.animateAppear()
+        }
     }
     
-    private func animateViaBasicAnimation(){
-        let animation = CABasicAnimation(keyPath: #keyPath(CALayer.transform))
-        animation.fromValue = layer.transform
-        animation.toValue = CATransform3DTranslate(layer.transform, 0, 300, 0)
-        
-    }
     
 }
--- AnimationsTask/Views/ColorView.swift
+++ AnimationsTask/Views/ColorView.swift
@@ -8,6 +8,20 @@
 
 import UIKit
 
+fileprivate struct Const {
+    
+    static let cornerRadiusAnimationDuration: Double = 0.25
+    static let cornerRadiusAnimationDelay: Double = 0.3
+    static let scaleAnimationKeyTimes: [NSNumber] = [0, 0.8, 1]
+    static let scaleAnimationMaxScale: CGFloat = 1.2
+    static let scaleAnimationDuration: Double = 0.5
+    
+    static let fadeAnimationDuration: Double = 0.5
+    
+    static let particlesAnimationDuration: Double = 1.0
+    static let particleAnimationTranslate = 500 * 2
+}
+
 @IBDesignable
 class ColorView: UIView {
     
@@ -37,12 +51,11 @@ class ColorView: UIView {
         }
     }
     
-    @IBInspectable var animationDuration: Double = 1.0
-    @IBInspectable var faded: Bool = false {
-        didSet {
-            self.isHidden = faded
-        }
-    }
+    
+    public fileprivate(set) var isVisible: Bool = true
+    fileprivate var isAnimatingNow: Bool = false
+    
+    public var visibilityObserver: ((Bool) -> Void)?
     
     //MARK: - Initialization
     required init?(coder aDecoder: NSCoder) {
@@ -57,7 +70,6 @@ class ColorView: UIView {
     
     private func commonInit(){
         updateMainLayer()
-        self.isHidden = faded
     }
     
     //MARK: - Private helper methods
@@ -80,9 +92,159 @@ class ColorView: UIView {
         }
     }
     
-    private func maxCornerRadius() -> Int {
+    func maxCornerRadius() -> Int {
         let minDimensionSize = self.frame.size.width > self.frame.size.height ? self.frame.size.height : self.frame.size.width
         return Int(minDimensionSize / 2)
     }
-
 }
+
+//MARK: - Animation methods
+
+extension ColorView {
+
+    func animateAppear(){
+
+        if(isAnimatingNow || isVisible){
+            return
+        }
+
+        isAnimatingNow = true
+
+        let currentCornerRadius = self.cornerRadius
+
+        class CompletionDelegate: NSObject, CAAnimationDelegate {
+
+            weak var view: ColorView?
+            let cornerRadius: Int
+
+            init(view: ColorView, cornerRadius: Int){
+                self.view = view
+                self.cornerRadius = cornerRadius
+            }
+
+            func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+                view?.cornerRadius = cornerRadius
+            }
+
+        }
+
+        self.cornerRadius = maxCornerRadius()
+        let cornerRadiusAnimation = CABasicAnimation(keyPath: #keyPath(CALayer.cornerRadius))
+        cornerRadiusAnimation.fromValue = maxCornerRadius()
+        cornerRadiusAnimation.toValue = currentCornerRadius
+        cornerRadiusAnimation.beginTime = CACurrentMediaTime() + Const.cornerRadiusAnimationDelay
+        cornerRadiusAnimation.duration = Const.cornerRadiusAnimationDuration
+        cornerRadiusAnimation.delegate = CompletionDelegate(view: self, cornerRadius: currentCornerRadius)
+
+        self.layer.add(cornerRadiusAnimation, forKey: "CornerRadius")
+
+
+        class AppearCompletionDelegate: NSObject, CAAnimationDelegate {
+
+            weak var view: ColorView?
+
+            init(view: ColorView){
+                self.view = view
+            }
+            
+            func animationDidStart(_ anim: CAAnimation) {
+                view?.layer.opacity = 1.0
+            }
+
+            func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+                view?.isAnimatingNow = false
+                view?.isVisible = true
+                view?.visibilityObserver?(true)
+            }
+
+        }
+
+
+        let scaleAnimation = CAKeyframeAnimation(keyPath: #keyPath(CALayer.transform))
+        let scaleTransforms: [CATransform3D] = [CATransform3DScale(self.layer.transform, 0.0, 0.0, 1),
+                                                CATransform3DScale(self.layer.transform,
+                                                                   Const.scaleAnimationMaxScale,
+                                                                   Const.scaleAnimationMaxScale,
+                                                                   1),
+                                                self.layer.transform]
+        scaleAnimation.values = scaleTransforms
+        scaleAnimation.keyTimes = Const.scaleAnimationKeyTimes
+        scaleAnimation.duration = Const.scaleAnimationDuration
+        scaleAnimation.delegate = AppearCompletionDelegate(view: self)
+        self.layer.add(scaleAnimation, forKey: "scale")
+    }
+
+    func animateDisappear(){
+
+        if(isAnimatingNow || !isVisible) {
+            return
+        }
+
+        isAnimatingNow = true
+
+        class CompletionDelegate: NSObject, CAAnimationDelegate {
+            weak var view: ColorView?
+            init(view: ColorView){
+                self.view = view
+            }
+            func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+                if let view = self.view {
+                    view.visibilityObserver?(true)
+                    view.isVisible = false
+                    let color = view.layer.backgroundColor
+                    view.layer.opacity = 0.0
+                    ColorView.animateParticles(view: view, particleColor: UIColor.red.cgColor)
+                }
+            }
+        }
+
+        let fadeAnimation = CAKeyframeAnimation(keyPath: #keyPath(CALayer.opacity))
+        fadeAnimation.duration = Const.fadeAnimationDuration
+        fadeAnimation.values = [1.0, 0.0]
+        fadeAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)]
+        fadeAnimation.delegate = CompletionDelegate(view: self)
+        self.layer.add(fadeAnimation, forKey: "FadeAnimation")
+
+
+
+    }
+
+    private static func animateParticles(view: ColorView, particleColor: CGColor){
+        class CompletionDelegate: NSObject, CAAnimationDelegate {
+            weak var layer: CALayer?
+            weak var view: ColorView?
+            init(layer: CALayer, view: ColorView){
+                self.layer = layer
+                self.view = view
+            }
+
+            func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+                layer?.removeFromSuperlayer()
+                view?.isAnimatingNow = false
+            }
+        }
+        //up, right, down, left
+        let xAndYKoeff = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+        for (xMult, yMult) in xAndYKoeff {
+            let layer = CALayer()
+            layer.backgroundColor = particleColor
+            layer.frame = view.bounds
+            layer.cornerRadius = 20
+            layer.setAffineTransform(CGAffineTransform(scaleX: 0.1, y: 0.1))
+            view.layer.addSublayer(layer)
+
+            let moveAnimation = CAKeyframeAnimation(keyPath: #keyPath(CALayer.transform))
+            let moveTransforms = [layer.transform,
+                                  CATransform3DTranslate(layer.transform,
+                                                         CGFloat(xMult * Const.particleAnimationTranslate),
+                                                         CGFloat(yMult * Const.particleAnimationTranslate),
+                                                         0)]
+            moveAnimation.values = moveTransforms
+            moveAnimation.delegate = CompletionDelegate(layer: layer, view: view)
+            moveAnimation.duration = Const.particlesAnimationDuration
+            moveAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)]
+            layer.add(moveAnimation, forKey: "MoveDirection: (\(xMult), \(yMult))")
+        }
+    }
+}
+
